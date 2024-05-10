09f1a2f05e34a071f122c95fcc1f3f36937c4125
kimsor1 <imoiksr@gmail.com>
2024-05-10 16:48:51 +0900

    스토리보드 Viewcontroller에 연결

--- teamProject/teamProject/ViewController.swift
+++ teamProject/teamProject/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfSearch: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnInsert(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnSearch(_ sender: UIButton) {
+    }
+    
 }
 
